40b5a76a496e4e6347d2b81d0f906ef027c89eee
GeoRPOliveira <geovanna.oliveira@germinare.org.br>
2024-07-04 10:03:28 -0300

    Tutor ou profissional

--- app/src/main/java/com/mobile/peticos/Login.java
+++ app/src/main/java/com/mobile/peticos/Login.java
@@ -2,7 +2,9 @@ package com.mobile.peticos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void TutorOuPeofissional(View view) {
+        Intent intent = new Intent(this, Tutor_ou_Profissional.class);
+        startActivity(intent);
     }
 }
