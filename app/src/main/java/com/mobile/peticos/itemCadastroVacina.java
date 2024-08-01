22ddecef9045e1d3a246cc0f501c435e6eddaf2b
liviagomesss <livia.gomes171207@gmail.com>
2024-08-01 10:42:53 -0300

    feat: vacinas

---
+++ app/src/main/java/com/mobile/peticos/itemCadastroVacina.java
@@ -0,0 +1,14 @@
+package com.mobile.peticos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class itemCadastroVacina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_cadastro_vacina);
+    }
+}
