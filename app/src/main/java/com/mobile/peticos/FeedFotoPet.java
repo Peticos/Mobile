1bfc318680085860bfde9c1c37fdce3a94db925f
liviagomesss <livia.gomes171207@gmail.com>
2024-08-09 10:52:20 -0300

    primeira versão da tela de vacinas

--- app/src/main/java/com/mobile/peticos/FeedFotoPet.java
+++ app/src/main/java/com/mobile/peticos/FeedFotoPet.java
@@ -9,6 +9,6 @@ public class FeedFotoPet extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed_foto_pet);
+        setContentView(R.layout.fragment_feed_do_pet);
     }
 }
