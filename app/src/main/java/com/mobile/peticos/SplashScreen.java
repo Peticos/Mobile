75c1c83ff74426e0b3e34a00f89e0e6a3fee894c
liviagomesss <livia.gomes171207@gmail.com>
2024-08-30 12:23:32 -0300

    commitando para poder dar pull

--- app/src/main/java/com/mobile/peticos/SplashScreen.java
+++ app/src/main/java/com/mobile/peticos/SplashScreen.java
@@ -21,7 +21,7 @@ public class SplashScreen extends AppCompatActivity {
     }
 
     private void abritela(){
-        Intent intent = new Intent(this, Login.class);
+        Intent intent = new Intent(this, perfil_pet.class);
         startActivity(intent);
         finish();
     }
